3b0524138eb767040832d405528b4fe406d91801
stars04 <starwas1234@yahoo.com>
2025-11-22 11:05:51 -0500

    Turns out it was not Extraneous Enable Pin Sets

--- src/main.c
+++ src/main.c
@@ -34,12 +34,18 @@ void write_4bits(uint8_t byte, uint8_t mode) {
   uint8_t low_bits = mode | ((byte << 4) & 0xF0) | LCD_BACKLIGHT;
 
   // Enable Pin required to write to Registers
-  i2c_write_byte(high_bits | LCD_ENABLE_PIN);
+  i2c_write_byte(high_bits);
 
+  i2c_write_byte(high_bits | LCD_ENABLE_PIN);
+  sleep_ms(1);
+  i2c_write_byte(high_bits & ~LCD_ENABLE_PIN);
   sleep_ms(1);
 
-  i2c_write_byte(low_bits | LCD_ENABLE_PIN);
+  i2c_write_byte(low_bits);
 
+  i2c_write_byte(low_bits | LCD_ENABLE_PIN);
+  sleep_ms(1);
+  i2c_write_byte(low_bits & ~LCD_ENABLE_PIN);
   sleep_ms(1);
 }
 
