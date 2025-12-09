95f6fd1b0699fe9964142e1012df5a84509d217c
stars04 <starwas1234@yahoo.com>
2025-12-09 18:44:29 -0500

    verbose comment added to lcd.h for lcd_print_address

--- inc/lcd.h
+++ inc/lcd.h
@@ -2,6 +2,10 @@ void lcd_initialization();
 
 void lcd_print(char* input_text);
 
+// lcd_print_address prints text starting at the specific memory ddram_address
+// i.e. in an unshifted display, writing to 0x8F (LINE_1_END) will set the 
+// cursor to the address that corresponds to the last visible character
+// on the top row in the unshifted display.
 void lcd_print_address(char* input_text, uint8_t ddram_address);
 
 void lcd_clear();
